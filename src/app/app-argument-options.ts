348f37b1ced7b20426b7706ebdd14da9fbc3e8fd
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-13 16:42:54 -0800

    WIP. help update definitions

--- src/app/app-argument-options.ts
+++ src/app/app-argument-options.ts
@@ -2,38 +2,37 @@ export const optionDefinitions = [
     {
         name: 'host',
         type: String,
-        description: 'Host name like "www.crossmatch.com"'
+        description: 'Host name like "www.crossmatch.com"',
     },
     {
         name: 'port',
         type: String,
-        description: 'SFTP port.'
+        description: 'SFTP port.',
     },
     {
         name: 'username',
-        alias: 'u',
         type: String,
-        description: 'User name for this session.'
+        description: 'User name for this session.',
+        alias: 'u',
     },
     {
         name: 'password',
-        alias: 'p',
         type: String,
-        description: 'User password.'
+        description: 'User password.',
+        alias: 'p',
     },
     {
         name: 'keyfile',
         type: String,
-        description: 'Path to key file. This will be expanded with environment variables.'
+        description: 'Path to key file. This will be expanded with environment variables.',
     },
     {
         name: 'key',
         type: String,
-        description: 'User key'
+        description: 'User key',
     },
     {
         name: 'ftp',
-        alias: 'f',
         type: String,
         multiple: true,
         description: `Each line must be in format: {gray <local> = <operation> = <remote>}
@@ -45,23 +44,24 @@ export const optionDefinitions = [
                 {gray     "<localPathAndFileName> = u = <remotePathAndFileName>"}
                 {gray     "<localPathAndFileName> = d = <remotePathAndFileName>"}
                 {gray     "<localPathAndFileName> = l = <remotePath>"}
-            * Existing local files will be overwritten silently.`
+            * Existing local files will be overwritten silently.`,
+        alias: 'f',
     },
     {
         name: 'alias',
-        alias: 'a',
         type: String,
         multiple: true,
         description: `Aliases to expand any path after start remote folder aquired from SFTP.
             For example:
                 {gray     --alias "root = \\{start\\}/files/crossmatch"}
                 {gray     --alias "g01 = \\{root\\}/AltusAddons/g01"}
-            The {underline start} alias is automatically added as the initial working directory of the SFTP server.`
+            The {underline start} alias is automatically added as the initial working directory of the SFTP server.`,
+        alias: 'a',
     },
     {
         name: 'help',
-        alias: 'h',
         type: Boolean,
-        description: 'Show this help screen.'
+        description: 'Show extended help.',
+        alias: 'h',
     }
 ];
