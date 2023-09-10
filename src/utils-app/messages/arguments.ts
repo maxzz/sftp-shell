90ce8870424e2a8a641daf1f3dcb2ab7abef9729
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-10 15:08:14 -0700

    WIP. CLI&API. update trace output

--- src/utils-app/messages/arguments.ts
+++ src/utils-app/messages/arguments.ts
@@ -2,9 +2,9 @@ import { CLIOptions } from "../../types";
 import chalk from "chalk";
 
 export function printCommandLineArguments() {
-    console.log(chalk.cyan(`\nCommand line arguments: \n${process.argv.map((str,idx)=>`    ${idx}: ${str}`).join('\n')}`));
+    console.log(chalk.yellow(`\nCommand line arguments: \n${process.argv.map((str,idx)=>`    ${idx}: ${str}`).join('\n')}`));
 }
 
 export function printCLIOptions(cliOptions: CLIOptions) {
-    console.log(chalk.cyan('\nargOptions = \n'), JSON.stringify(cliOptions, null, 4), '\n');
+    console.log(chalk.yellow(`\nParsed options: ${JSON.stringify(cliOptions, null, 4)}`));
 }
