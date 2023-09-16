0643fcda8c362192e1b8c8ab76d6577875550779
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-16 15:10:23 -0700

    WIP. CLI&API. add pnpm dependencies

--- src/utils/utils-os-files/04-file-read-write.ts
+++ src/utils/utils-os-files/04-file-read-write.ts
@@ -15,7 +15,7 @@ export function readJson(fname: string): any {
 export function writeJson(fname: string, obj: any): void {
     try {
         mkDirSync(path.dirname(fname));
-        fs.writeFileSync(fname, JSON.stringify(obj, null, 4));;
+        fs.writeFileSync(fname, JSON.stringify(obj, null, 4));
     } catch (error) {
         console.error('\nFailed to write JSON:', fname, error);
         throw error;
